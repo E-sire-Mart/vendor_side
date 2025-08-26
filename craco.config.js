89b6f2a28332709cbbea147c13f8f4c5b3f144ec
lutopidop <luopidkkop@gmail.com>
2025-08-25 23:44:18 -0700

    hosting problem fix

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,32 @@
+const path = require('path');
+
 module.exports = {
   webpack: {
-    configure: (webpackConfig) => {
-      webpackConfig.ignoreWarnings = [
-        {
-          module: /stylis-plugin-rtl/,
-          message: /Failed to parse source map/,
-        },
-      ];
-      return webpackConfig;
-    },
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@layouts': path.resolve(__dirname, 'src/layouts'),
+      '@auth': path.resolve(__dirname, 'src/auth'),
+      '@examples': path.resolve(__dirname, 'src/examples'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@context': path.resolve(__dirname, 'src/context'),
+      '@redux': path.resolve(__dirname, 'src/redux')
+    }
   },
+  devServer: {
+    port: 3001,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3003',
+        changeOrigin: true,
+        secure: false
+      },
+      '/socket.io': {
+        target: 'http://localhost:3003',
+        changeOrigin: true,
+        ws: true
+      }
+    }
+  }
 };
